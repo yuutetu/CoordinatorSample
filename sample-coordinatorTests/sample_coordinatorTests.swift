8ada4552d39de5f3de6a539a217090b40fe801d4
加賀江 優幸 <yuutetu@kagakousachinoMacBook-Pro.local>
2017-12-24 09:00:40 +0900

    Initial Commit

---
+++ sample-coordinatorTests/sample_coordinatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  sample_coordinatorTests.swift
+//  sample-coordinatorTests
+//
+//  Created by 加賀江 優幸 on 2017/12/24.
+//  Copyright © 2017年 yuutetu. All rights reserved.
+//
+
+import XCTest
+@testable import sample_coordinator
+
+class sample_coordinatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
